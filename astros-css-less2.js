ff6799bc018f7406be5a5641af40bf795beb6c86
tiger <wanhu88@gmail.com>
2016-06-02 10:12:54 +0800

    修改为默认压缩

--- astros-css-less2.js
+++ astros-css-less2.js
@@ -14,7 +14,7 @@ module.exports = new astro.Middleware({
     let project = asset.project;
     let prjCfg = astro.getProject(project);
     // 分析JS模块的依赖
-    let isCompress = prjCfg.compressCss || this.config.compress;
+    let isCompress = prjCfg.compressCss || this.config.compress != false;
     let webComCode = '';
     let components = [];
     if (asset.components && asset.components.length) {
